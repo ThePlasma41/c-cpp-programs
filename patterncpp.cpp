c3f9e8585951de5cc26bc4335cd85d8cd575b7c4
ThePlasma41 <mahin.mr009@gmail.com>
2025-09-16 12:30:53 +0600

    patterncpp.cpp
    A program for printing a nested square rectangle pattern alternating its outer boarder with '*' or '#' till the center.

--- patterncpp.cpp
+++ patterncpp.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 using namespace std;
 int main(void) {
@@ -6,45 +5,37 @@ int main(void) {
     cin>>n;
 
     int x=(n-1)/2;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-
-          if(i<=(n-1)/2)
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+           if(i<=(n-1)/2)
               {
                   if(j>=i && j<=n-1-i)
                   {
                        if(i%2==0) cout<<'*';
-                       else cout<<'#';//direct print
+                       else cout<<'#';
                    }
-                  else{
-                        if(j%2==0)cout<<'*'; //alternate
+                  else{  //alternate
+                        if(j%2==0)cout<<'*'; 
                         else cout<<'#';
                       }
-
               }
-
-         else{
-               if(j>=x-k && j<=x+k)
+          else{
+                if(j>=x-k && j<=x+k)
                {
-                  if(i%2==0) cout<<'*';
-                  else cout<<'#';
+                    if(i%2==0)  cout<<'*';
+                    else        cout<<'#';
                }
                else
-               {
-                 if(j%2==0)cout<<'*';
-                 else cout<<'#';
+               {   //alternate
+                    if(j%2==0)  cout<<'*';
+                    else        cout<<'#';
                }
-
-
-
-             }
-
-
         }
-               if(i>x)k++;
-        cout<<endl; //cout<<'x'<<x<<'k'<<k<<endl;
-
-
-    }
-
+      }
+        if(i>x)k++;
+        cout<<endl;
+     }
 }
+
