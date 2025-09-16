3056bc385e528f1525bc118f938d58cb9e174e1d
ThePlasma41 <mahin.mr009@gmail.com>
2025-09-16 11:49:23 +0600

    Add files via upload

---
+++ patterncpp.cpp
@@ -0,0 +1,50 @@
+
+#include <iostream>
+using namespace std;
+int main(void) {
+    int n,k=1;
+    cin>>n;
+
+    int x=(n-1)/2;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+
+          if(i<=(n-1)/2)
+              {
+                  if(j>=i && j<=n-1-i)
+                  {
+                       if(i%2==0) cout<<'*';
+                       else cout<<'#';//direct print
+                   }
+                  else{
+                        if(j%2==0)cout<<'*'; //alternate
+                        else cout<<'#';
+                      }
+
+              }
+
+         else{
+               if(j>=x-k && j<=x+k)
+               {
+                  if(i%2==0) cout<<'*';
+                  else cout<<'#';
+               }
+               else
+               {
+                 if(j%2==0)cout<<'*';
+                 else cout<<'#';
+               }
+
+
+
+             }
+
+
+        }
+               if(i>x)k++;
+        cout<<endl; //cout<<'x'<<x<<'k'<<k<<endl;
+
+
+    }
+
+}
